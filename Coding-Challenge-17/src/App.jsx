0a6810e6b32a751b27358882c2f5a8dc74354328
Lina <ambowers@usf.edu>
2024-11-23 19:43:13 -0500

    Setup root component with global state and routing.

---
+++ Coding-Challenge-17/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+      <div>
+          <h1>Tour Comparison App</h1>
+      </div>
+  );
+};
+
+export default App
